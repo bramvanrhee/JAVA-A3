f2ff3c1a57b8aade42098e72b89fb015c7211b84
bramvanrhee <47520024+bramvanrhee@users.noreply.github.com>
2019-09-27 10:25:13 +0200

    Voortgang Eindopdracht 1

--- src/Eindopdracht1/MainPanel.java
+++ src/Eindopdracht1/MainPanel.java
@@ -1,19 +1,25 @@
 package Eindopdracht1;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 import javax.swing.*;
 
 public class MainPanel extends JPanel {
 	
 	int i = 0;
+	GameBoard gameBoard;
 	
 	public MainPanel() {
+		gameBoard = new GameBoard();
 		setLayout(new GridLayout(4, 4));
-		
-		for (int i = 0; i < 16; i++) {
-		add(new memoryButton().buttons[i] = new JButton("Card" + i));
+		ArrayList<Card> list = gameBoard.getList();
+		for (Card card : list) {
+			add(card);
 		}
+		
+		
+		
 		
 		
 	}
--- src/Eindopdracht1/main.java
+++ src/Eindopdracht1/main.java
@@ -1,6 +1,6 @@
 package Eindopdracht1;
 
-public class main {
+public class Main {
 	
 	public static void main(String[] args) {
 		MainWindow mainWindow = new MainWindow();
